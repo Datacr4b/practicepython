a8b4c353c4c91d7666143427f31f5226ebebd3e1
LittleMewtwo <kamil.ejjennane@yahoo.com>
2025-03-22 11:03:19 +0100

    4th Commit! More practice !

---
+++ Practice Programs/File practice.py
@@ -0,0 +1,7 @@
+# File practice
+
+with open('employees.txt', 'r') as rf:
+    with open('copy_employees.txt', 'w') as wf:
+        chunk_size = 20
+
+
--- Practice Programs/OS module practice.py
+++ Practice Programs/OS module practice.py
@@ -1,5 +1,8 @@
 import os
 
-os.chdir('/Users/julia.menshikov/Desktop')
-
-print(os.listdir())
+folder = input("Type in the Folder path: ")
+filter_size = int(input("Type in the size filter: "))
+for file in os.listdir(folder):
+    size = os.stat(os.path.join(folder,file)).st_size
+    if size > filter_size:
+        print(f"{file}: {size} bytes")
